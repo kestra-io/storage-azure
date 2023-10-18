10379d8806c0a2951cb8b88199bc25f7f83b45cf
Martin GUIBERT <mguibert@kestra.io>
2023-10-18 15:33:28 +0200

    feat: multi file editor
    Implement new interface for multifile editor

---
+++ src/main/java/io/kestra/storage/azure/AzureFileAttributes.java
@@ -0,0 +1,38 @@
+package io.kestra.storage.azure;
+
+import com.azure.storage.blob.models.BlobProperties;
+import io.kestra.core.storages.FileAttributes;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class AzureFileAttributes implements FileAttributes {
+
+    String fileName;
+    BlobProperties properties;
+    boolean isDirectory;
+
+    @Override
+    public long getLastModifiedTime() {
+        return properties.getLastModified().toEpochSecond();
+    }
+
+    @Override
+    public long getCreationTime() {
+        return properties.getCreationTime().toEpochSecond();
+    }
+
+    @Override
+    public FileType getType() {
+        if (isDirectory) {
+            return FileType.Directory;
+        }
+        return FileType.File;
+    }
+
+    @Override
+    public long getSize() {
+        return properties.getBlobSize();
+    }
+}
--- src/main/java/io/kestra/storage/azure/AzureStorage.java
+++ src/main/java/io/kestra/storage/azure/AzureStorage.java
@@ -1,22 +1,26 @@
 package io.kestra.storage.azure;
 
+import com.azure.core.util.polling.SyncPoller;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
-import com.azure.storage.blob.models.BlobStorageException;
-import com.azure.storage.blob.models.ListBlobsOptions;
-import com.google.common.collect.Streams;
+import com.azure.storage.blob.models.*;
+import io.kestra.core.storages.FileAttributes;
 import io.kestra.core.storages.StorageInterface;
 import io.micronaut.core.annotation.Introspected;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import org.apache.commons.lang3.StringUtils;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.nio.file.Path;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 import static io.kestra.core.utils.Rethrow.throwFunction;
 
@@ -24,6 +28,7 @@ import static io.kestra.core.utils.Rethrow.throwFunction;
 @AzureStorageEnabled
 @Introspected
 public class AzureStorage implements StorageInterface {
+    private static final String DIRECTORY_MARKER_FILE = ".kestradirectory";
     @Inject
     AzureClientFactory factory;
 
@@ -49,8 +54,34 @@ public class AzureStorage implements StorageInterface {
 
             return blobClient.openInputStream();
         } catch (BlobStorageException e) {
-            throw new IOException(e);
+            throw reThrowBlobStorageException(uri, e);
         }
+    }
+
+    @Override
+    public List<FileAttributes> list(String tenantId, URI uri) throws IOException {
+        String path = getPath(tenantId, uri);
+        String prefix = (path.endsWith("/")) ? path : path + "/";
+
+        if (!client().getBlobClient(prefix).exists()) {
+            throw new FileNotFoundException(uri + " (Not Found)");
+        }
+
+        ListBlobsOptions listBlobsOptions = new ListBlobsOptions()
+            .setPrefix(prefix)
+            .setDetails(new BlobListDetails().setRetrieveDeletedBlobs(false).setRetrieveSnapshots(false));
+
+        return this.client().listBlobsByHierarchy("/", listBlobsOptions, null).stream()
+            .map(BlobItem::getName)
+            .filter(key -> {
+                key = "/" + key;
+                key = key.substring(prefix.length());
+                // Remove recursive result and requested dir
+                return !key.isEmpty() && !Objects.equals(key, prefix)
+                    && !key.endsWith(DIRECTORY_MARKER_FILE);
+            })
+            .map(throwFunction(this::getFileAttributes))
+            .toList();
     }
 
     @Override
@@ -74,7 +105,7 @@ public class AzureStorage implements StorageInterface {
 
             return blobClient.getProperties().getBlobSize();
         } catch (BlobStorageException e) {
-            throw new IOException(e);
+            throw reThrowBlobStorageException(uri, e);
         }
     }
 
@@ -89,39 +120,120 @@ public class AzureStorage implements StorageInterface {
 
             return blobClient.getProperties().getLastModified().toInstant().toEpochMilli();
         } catch (BlobStorageException e) {
-            throw new IOException(e);
+            throw reThrowBlobStorageException(uri, e);
         }
+    }
+
+    @Override
+    public FileAttributes getAttributes(String tenantId, URI uri) throws IOException {
+        String path = getPath(tenantId, uri);
+        return getFileAttributes(path);
+    }
+
+    private FileAttributes getFileAttributes(String path) throws FileNotFoundException {
+        String fileName = Path.of(path).getFileName().toString();
+
+        BlobClient blobClient = this.client().getBlobClient(path);
+        if (!path.endsWith("/")) {
+            BlobClient dirBlobClient = this.client().getBlobClient(path + "/" + DIRECTORY_MARKER_FILE);
+            if (dirBlobClient.exists()) {
+                path += "/";
+                blobClient = dirBlobClient;
+            }
+        }
+        if (!blobClient.exists()) {
+            throw new FileNotFoundException(fileName + " (File not found)");
+        }
+
+        return AzureFileAttributes.builder()
+            .fileName(fileName)
+            .isDirectory(path.endsWith("/") || path.endsWith(DIRECTORY_MARKER_FILE))
+            .properties(blobClient.getProperties())
+            .build();
     }
 
     @Override
     public URI put(String tenantId, URI uri, InputStream data) throws IOException {
         try {
-            BlobClient blobClient = this.blob(getURI(tenantId, uri));
-
+            URI path = getURI(tenantId, uri);
+            BlobClient blobClient = this.blob(path);
+            mkdirs(path.getPath());
             try (data) {
                 blobClient.upload(data, true);
             }
 
             return URI.create("kestra://" + uri.getPath());
         } catch (BlobStorageException e) {
-            throw new IOException(e);
+            throw reThrowBlobStorageException(uri, e);
         }
     }
 
     public boolean delete(String tenantId, URI uri) throws IOException {
-        try {
-            BlobClient blobClient = this.blob(getURI(tenantId, uri));
-
-            if (!blobClient.exists()) {
-                return false;
-            }
-
-            blobClient.delete();
-
-            return true;
-        } catch (BlobStorageException e) {
-            throw new IOException(e);
+        String path = getPath(tenantId, uri);
+        if (isDir(path)) {
+            return !deleteByPrefix(tenantId, uri).isEmpty();
         }
+        BlobClient blobClient = this.client().getBlobClient(path);
+        if (!blobClient.exists()) {
+            return false;
+        }
+        blobClient.delete();
+        return true;
+    }
+
+    @Override
+    public URI createDirectory(String tenantId, URI uri) throws IOException {
+        String path = getPath(tenantId, uri);
+        if (!StringUtils.endsWith(path, "/")) {
+            path += "/";
+        }
+        mkdirs(path + DIRECTORY_MARKER_FILE);
+        return URI.create("kestra://" + uri.getPath());
+    }
+
+    private void mkdirs(String path) throws IOException {
+        if (!path.endsWith("/") && path.lastIndexOf('/') > 0) {
+            path = path.substring(0, path.lastIndexOf('/') + 1);
+        }
+
+        path += DIRECTORY_MARKER_FILE;
+
+        try {
+            BlobClient blobClient = this.blob(URI.create(path));
+            blobClient.upload(new ByteArrayInputStream(new byte[]{}), true);
+        } catch (BlobStorageException e) {
+            throw reThrowBlobStorageException(URI.create(path), e);
+        }
+    }
+
+    @Override
+    public URI move(String tenantId, URI from, URI to) throws IOException {
+        if (!exists(tenantId, from)) {
+            throw new FileNotFoundException(from + " (File not found)");
+        }
+
+        String source = getPath(tenantId, from);
+        String dest = getPath(tenantId, to);
+
+        BlobContainerClient client = this.client();
+
+        ListBlobsOptions listBlobsOptions = new ListBlobsOptions()
+            .setPrefix(getPath(tenantId, from))
+            .setDetails(new BlobListDetails().setRetrieveDeletedBlobs(false).setRetrieveSnapshots(false));
+        for (BlobItem itemResult : client.listBlobs(listBlobsOptions, Duration.ofSeconds(30))) {
+            if (isDir(itemResult.getName())) {
+                // do not copy directories
+                continue;
+            }
+            String destName = dest + itemResult.getName().substring(source.substring(1).length());
+            mkdirs(dest);
+            BlobClient sourceClient = client.getBlobClient(itemResult.getName());
+            SyncPoller<BlobCopyInfo, Void> poller = client.getBlobClient(destName).beginCopy(sourceClient.getBlobUrl(),
+                Duration.ofSeconds(30));
+            poller.waitForCompletion();
+        }
+        deleteByPrefix(tenantId, from);
+        return URI.create("kestra://" + from.getPath());
     }
 
     @Override
@@ -129,41 +241,79 @@ public class AzureStorage implements StorageInterface {
         try {
             BlobContainerClient client = this.client();
 
-            ListBlobsOptions listBlobsOptions = new ListBlobsOptions();
-            listBlobsOptions.setPrefix(getURI(tenantId, storagePrefix).getPath());
+            String path = getPath(tenantId, storagePrefix);
+            if (!path.endsWith("/")) {
+                path += "/";
+            }
+            ListBlobsOptions listBlobsOptions = new ListBlobsOptions()
+                .setPrefix(path)
+                .setDetails(new BlobListDetails().setRetrieveDeletedBlobs(false).setRetrieveSnapshots(false));
 
-            List<String> deleted = Streams
-                .stream(client.listBlobs(listBlobsOptions, Duration.ofSeconds(30)))
-                .filter(blobItem -> blobItem.getProperties().getContentType() != null)
-                .map(throwFunction(itemResult -> {
-                    try {
-                        return itemResult.getName();
-                    } catch (Throwable e) {
-                        throw new IOException(e);
-                    }
-                }))
-                .collect(Collectors.toList());
+            List<String> deleted = new ArrayList<>();
+            List<String> directories = new ArrayList<>();
+            for (BlobItem itemResult : client.listBlobs(listBlobsOptions, Duration.ofSeconds(30))) {
+                String name = itemResult.getName();
+                Boolean isDir = isDir(name);
+                if (isDir) {
+                    directories.add(name);
+                    continue;
+                }
+                BlobClient blobClient = this.client().getBlobClient(name);
+                blobClient.delete();
+                if (!name.endsWith(DIRECTORY_MARKER_FILE)) {
+                    // We do not want to output the hidden file we use to mark directory as deleted
+                    deleted.add(name);
+                }
+            }
 
-            deleted
-                .forEach(s -> {
-                    BlobClient blobClient = this.blob(URI.create(s));
-                    blobClient.delete();
-                });
+            // also remove main directory (we have to remove start and ending /)
+            directories.add(path.substring(1, path.length() - 1));
+            directories.sort((s1, s2) -> s2.length() - s1.length());
+            for (String directory : directories) {
+                BlobClient blobClient = this.client().getBlobClient(directory);
+                blobClient.delete();
+                deleted.add(directory);
+            }
 
-
-            return deleted
-                .stream()
+            return deleted.stream()
                 .map(s -> URI.create("kestra:///" + s.replace(tenantId + "/", "")))
-                .collect(Collectors.toList());
+                .toList();
         } catch (BlobStorageException e) {
+            if (e.getErrorCode() == BlobErrorCode.BLOB_NOT_FOUND || e.getErrorCode() == BlobErrorCode.RESOURCE_NOT_FOUND) {
+                return List.of();
+            }
             throw new IOException(e);
         }
     }
 
-    private URI getURI(String tenantId, URI uri) {
-        if (tenantId == null) {
-            return URI.create(uri.getPath());
+    private Boolean isDir(String path) {
+        // To check if a path is in fact a directory we check if our hidden file exists inside it
+        return this.client().getBlobClient(path + "/" + DIRECTORY_MARKER_FILE).exists();
+    }
+
+    private IOException reThrowBlobStorageException(URI storagePrefix, BlobStorageException e) {
+        if (e.getErrorCode() == BlobErrorCode.BLOB_NOT_FOUND || e.getErrorCode() == BlobErrorCode.RESOURCE_NOT_FOUND) {
+            return new FileNotFoundException(storagePrefix + " (File not found)");
         }
-        return URI.create("/" + tenantId + uri.getPath());
+        return new IOException(e);
+    }
+
+    private URI getURI(String tenantId, URI uri) {
+        return URI.create(getPath(tenantId, uri));
+    }
+
+    private String getPath(String tenantId, URI uri) {
+        parentTraversalGuard(uri);
+        if (tenantId == null) {
+            return uri.getPath();
+        }
+        return "/" + tenantId + uri.getPath();
+    }
+
+    // Traversal does not work with azure but it just return empty objects so throwing is more explicit
+    private void parentTraversalGuard(URI uri) {
+        if (uri.toString().contains("..")) {
+            throw new IllegalArgumentException("File should be accessed with their full path and not using relative '..' path.");
+        }
     }
 }
