3ad29cc2f4e0343d315f292c9a4fe3ecb7fa8e07
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-01-20 21:33:11 +0100

    fix: move application config to application-test.yml

--- src/test/java/io/kestra/storage/azure/AzureStorageTest.java
+++ src/test/java/io/kestra/storage/azure/AzureStorageTest.java
@@ -1,6 +1,8 @@
 package io.kestra.storage.azure;
 
 import io.kestra.core.storage.StorageTestSuite;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 
+@MicronautTest(environments = "secrets")
 class AzureStorageTest extends StorageTestSuite {
 }
