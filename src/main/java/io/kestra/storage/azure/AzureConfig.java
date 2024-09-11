c641002c2c8ab9baccf83677653c63481100b2b6
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-09-11 15:39:25 +0200

    fix: add config properties to handle ManagedIdentity credential

--- src/main/java/io/kestra/storage/azure/AzureConfig.java
+++ src/main/java/io/kestra/storage/azure/AzureConfig.java
@@ -1,5 +1,6 @@
 package io.kestra.storage.azure;
 
+import com.azure.identity.DefaultAzureCredentialBuilder;
 import io.kestra.core.models.annotations.PluginProperty;
 
 public interface AzureConfig {
@@ -21,4 +22,27 @@ public interface AzureConfig {
 
     @PluginProperty
     String getSasToken();
+
+    /**
+     * Specifies the client ID of user assigned or system assigned identity,
+     * when this credential is running in an environment with managed identities.
+     * <p>
+     * If unset, the value in the AZURE_CLIENT_ID environment variable will be used.
+     * </p>
+     * @see DefaultAzureCredentialBuilder#managedIdentityClientId(String)
+     */
+    @PluginProperty
+    String getManagedIdentityClientId();
+
+    /**
+     * Specifies the resource ID of user assigned or system assigned identity,
+     * when this credential is running in an environment with managed identities.
+     *
+     * <p>
+     * If unset, the value in the AZURE_CLIENT_ID environment variable will be used.
+     * </p>
+     * @see DefaultAzureCredentialBuilder#managedIdentityResourceId(String)
+     */
+    @PluginProperty
+    String getManagedIdentityResourceId();
 }
